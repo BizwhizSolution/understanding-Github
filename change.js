be5633841711d0ecc9f9de4dcd0e81608473a883
IgonorEmmanuel <Igonoree@gmail.com>
2022-09-23 11:43:30 +0100

    version 12

--- change.js
+++ change.js
@@ -1,3 +1,4 @@
 console.log("changed again");
 console.log("we keep learning");
 
+console.log("No changes");
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log('index.js');
-console.log('version 101');
+console.log('version 12');
 console.log("Gitting along")
