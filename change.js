b58bfa61c90876c0a671495183cb70a99324a1fe
IgonorEmmanuel <Igonoree@gmail.com>
2022-09-23 12:16:57 +0100

    version 13

--- change.js
+++ change.js
@@ -2,3 +2,4 @@ console.log("changed again");
 console.log("we keep learning");
 
 console.log("No changes");
+console.log("working on the remote files");
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log('index.js');
-console.log('version 12');
-console.log("Gitting along")
+console.log('version 13');
+console.log("Gitting along");
